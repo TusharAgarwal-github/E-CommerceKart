d2af1ab09580b986e8e7e04f8a43f26ffcaba0d3
Tushar10agarwal <62994734+TusharAgarwal-github@users.noreply.github.com>
2021-02-02 12:43:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './navbar/navbar'
+import SideBar from './sidebar/sidebar'
+
+function App() {
+	return (
+		<div class='sb-nav-fixed'>
+			<NavBar></NavBar>
+			<SideBar></SideBar>
+		</div>
+	)
+}
+
+export default App
